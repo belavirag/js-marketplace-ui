2c2a462815027502ffdc3e3de3660c71a2c0f469
belavirag <139229267+belavirag@users.noreply.github.com>
2023-11-21 22:08:18 +0100

    cleanup render function

--- src/js/components/card.js
+++ src/js/components/card.js
@@ -1,16 +1,4 @@
-
-/*
-<div class="col mb-4">
-          <div class="card">
-            <img src="https://dummyimage.com/600x400/000/fff.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-*/
+import { createElementHelper } from "../domutils.js";
 
 /**
  * 
@@ -22,50 +10,32 @@
  * @param {string}      card.contact 
  */
 export const render = ({ target, title, description, image, contact }) => {
-    const mainDiv = document.createElement("div");
-    mainDiv.classList.add("col", "mb-4");
-    const cardDiv = document.createElement("div");
-    cardDiv.classList.add("card");
+    const mainDiv = createElementHelper("div", "", ["col", "mb-4"]);
+    const cardDiv = createElementHelper("div", "", ["card"]);
     mainDiv.appendChild(cardDiv);
-    const img = document.createElement("img");
+    const img = createElementHelper("img");
     img.src = image;
     img.alt = title;
     cardDiv.appendChild(img);
 
-    const cardBodyDiv = document.createElement("div");
-    cardBodyDiv.classList.add("card-body");
-
-    const cardTitle = document.createElement("h5");
-    cardTitle.innerText = title;
-    cardTitle.classList.add("card-title");
+    const cardBodyDiv = createElementHelper("div", "", ["card-body"]);
+    const cardTitle = createElementHelper("h5", title, ["card-title"]);
     cardBodyDiv.appendChild(cardTitle);
-    const cardText = document.createElement("p");
-    cardText.innerText = description;
-    cardText.classList.add("card-text");
+    const cardText = createElementHelper("p", description, ["card-text"]);
     cardBodyDiv.appendChild(cardText);
-    const cardButton = document.createElement("button");
-    cardButton.innerText = "Details";
-    cardButton.classList.add("btn", "btn-primary");
-
-    const contactDetails = document.createElement("p");
-    contactDetails.classList.add("card-text");
-    contactDetails.innerText = contact;
+    const cardButton = createElementHelper("button", "Details", ["btn", "btn-primary"]);
+    const contactDetails = createElementHelper("p", contact, ["card-text"]);
     contactDetails.hidden = true;
     cardBodyDiv.appendChild(contactDetails);
 
     cardButton.addEventListener('click', (e) => {
-        const info = e.target.parentElement.children[2];
+        const infoElem = e.target.parentElement.children;
+        const info = infoElem[infoElem.length - 2];
+
         info.hidden = !info.hidden;
-        
-        if (info.hidden) {
-            e.target.innerText = "Details";
-        } else {
-            e.target.innerText = "Hide";
-        }
-        
+        e.target.innerText = info.hidden ? "Details" : "Hide";
     });
     cardBodyDiv.appendChild(cardButton);
-
 
     cardDiv.appendChild(cardBodyDiv);
     target.appendChild(mainDiv);
--- src/js/domutils.js
+++ src/js/domutils.js
@@ -1,1 +1,13 @@
-export const testing = () => alert("hi");
+/**
+ * @param {string} type
+ * @param {string} innerText 
+ * @param {string[]} classList 
+ * 
+ */
+export const createElementHelper = (type, innerText = "", classList = []) => {
+    const elem = document.createElement(type);
+    elem.innerText = innerText;
+    classList.forEach(cls => elem.classList.add(cls));
+
+    return elem;
+}
